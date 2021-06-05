ea1f9ea525056baf1655c5fcce688b50d716e213
Leonel Aguirre <leonel.aguirre@hbr.org>
2021-06-05 09:41:27 -0500

    :sparkles: NavBar component added.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home/Home";
+import NavBar from "./NavBar/NavBar";
 
 const App = () => {
   return (
     <Router>
+      <NavBar />
       <Switch>
         <Route path="/alt-path">
           <h1>Hello There!</h1>
         </Route>
         <Route path="/">
-          <h1>HOME</h1>
+          <Home />
         </Route>
       </Switch>
     </Router>
