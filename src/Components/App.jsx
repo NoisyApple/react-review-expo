b0bea765f086d8e583d7155190e5c5037510576f
Leonel Aguirre <leonel.aguirre@hbr.org>
2021-06-05 09:35:06 -0500

    :sparkles: React router added and working.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <h1>Hello There!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/alt-path">
+          <h1>Hello There!</h1>
+        </Route>
+        <Route path="/">
+          <h1>HOME</h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
