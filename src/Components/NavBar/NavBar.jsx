400021b6399f0d57e75929fdd89a8b412a9b899a
Leonel Aguirre <leonel.aguirre@hbr.org>
2021-06-05 09:54:30 -0500

    :sparkles: Color pallete added to scss core.

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,11 @@
+import "./NavBar.scss";
+
 import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className="nav-bar">
       <ul>
         <li>
           <Link to="/">HOME</Link>
